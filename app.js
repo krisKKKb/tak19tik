f2d6f4f0cb7d1759c35b10859a0efa42f2ea3398
kriskkkb <krisburk577@gmail.com>
2020-10-21 13:00:30 +0300

    fefe

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let gameBoard = [
     ['-', '-', '-']
 ]
 
+let gameOver = false
 const cellDivs = document.querySelectorAll('.cell')
 
 cellDivs.forEach(el => {
@@ -19,16 +20,46 @@ cellDivs.forEach(el => {
 
             if (gameBoard[0][0] == 'X' && gameBoard[0][1] == 'X' && gameBoard[0][2] == 'X') {
                 console.log('player win')
+            } else {
+                gameOver = true
             }
+            if
 
             if (gameBoard[1][0] == 'X' && gameBoard[1][1] == 'X' && gameBoard[1][2] == 'X') {
-                console.log('player win')
-            }
-
+                    console.log('player win')
+                } else {
+                    gameOver = true
+                }
             if (gameBoard[2][0] == 'X' && gameBoard[2][1] == 'X' && gameBoard[2][2] == 'X') {
                 console.log('player win')
+            } else {
+                gameOver = true
             }
-
+            if (gameBoard[0][0] == 'X' && gameBoard[1][1] == 'X' && gameBoard[2][2] == 'X') {
+                console.log('Player win')
+            } else {
+                gameOver = true
+            }
+            if (gameBoard[0][2] == 'X' && gameBoard[1][1] == 'X' && gameBoard[2][0] == 'X') {
+                console.log('Player win')
+            } else {
+                gameOver = true
+            }
+            if (gameBoard[0][1] == 'X' && gameBoard[1][1] == 'X' && gameBoard[2][1] == 'X') {
+                console.log('Player win')
+            } else {
+                gameOver = true
+            }
+            if (gameBoard[0][0] == 'X' && gameBoard[1][0] == 'X' && gameBoard[2][0] == 'X') {
+                console.log('Player win')
+            } else {
+                gameOver = true
+            }
+            if (gameBoard[0][2] == 'X' && gameBoard[1][2] == 'X' && gameBoard[2][2] == 'X') {
+                console.log('Player win')
+            } else {
+                gameOver = true
+            }
             console.log(gameBoard)
         }
     })
