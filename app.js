a9df95e7d6273cf3237628e4269fa4e0de34476d
kriskkkb <krisburk577@gmail.com>
2020-10-21 14:09:21 +0300

    pederastid ei roti koodi

--- app.js
+++ app.js
@@ -15,52 +15,61 @@ cellDivs.forEach(el => {
 
         if (gameBoard[selectedRow][selectedCol] == '-') {
             selectedCell.innerText = 'X'
-
             gameBoard[selectedRow][selectedCol] = 'X'
-
-            if (gameBoard[0][0] == 'X' && gameBoard[0][1] == 'X' && gameBoard[0][2] == 'X') {
-                console.log('player win')
-            } else {
-                gameOver = true
-            }
-            if
-
-            if (gameBoard[1][0] == 'X' && gameBoard[1][1] == 'X' && gameBoard[1][2] == 'X') {
-                    console.log('player win')
-                } else {
-                    gameOver = true
-                }
-            if (gameBoard[2][0] == 'X' && gameBoard[2][1] == 'X' && gameBoard[2][2] == 'X') {
-                console.log('player win')
-            } else {
-                gameOver = true
-            }
-            if (gameBoard[0][0] == 'X' && gameBoard[1][1] == 'X' && gameBoard[2][2] == 'X') {
-                console.log('Player win')
-            } else {
-                gameOver = true
-            }
-            if (gameBoard[0][2] == 'X' && gameBoard[1][1] == 'X' && gameBoard[2][0] == 'X') {
-                console.log('Player win')
-            } else {
-                gameOver = true
-            }
-            if (gameBoard[0][1] == 'X' && gameBoard[1][1] == 'X' && gameBoard[2][1] == 'X') {
-                console.log('Player win')
-            } else {
-                gameOver = true
-            }
-            if (gameBoard[0][0] == 'X' && gameBoard[1][0] == 'X' && gameBoard[2][0] == 'X') {
-                console.log('Player win')
-            } else {
-                gameOver = true
-            }
-            if (gameBoard[0][2] == 'X' && gameBoard[1][2] == 'X' && gameBoard[2][2] == 'X') {
-                console.log('Player win')
-            } else {
-                gameOver = true
-            }
-            console.log(gameBoard)
         }
+        if (gameBoard[0][0] == 'X') {
+            document.getElementById("02").innerText = "O";
+        }
+        if (gameBoard[0][1] == 'X') {
+            document.getElementById("00").innerText = "O";
+        }
+        if (gameBoard[0][2] == 'X') {
+            document.getElementById("10").innerText = "O";
+        }
+        if (gameBoard[1][0] == 'X') {
+            document.getElementById("20").innerText = "O";
+        }
+        if (gameBoard[0][0] == 'X' && gameBoard[0][1] == 'X' && gameBoard[0][2] == 'X') {
+            console.log('player win')
+        } else {
+            gameOver = true
+        }
+        if (gameBoard[1][0] == 'X' && gameBoard[1][1] == 'X' && gameBoard[1][2] == 'X') {
+            console.log('player win')
+        } else {
+            gameOver = true
+        }
+        if (gameBoard[2][0] == 'X' && gameBoard[2][1] == 'X' && gameBoard[2][2] == 'X') {
+            console.log('player win')
+        } else {
+            gameOver = true
+        }
+        if (gameBoard[0][0] == 'X' && gameBoard[1][1] == 'X' && gameBoard[2][2] == 'X') {
+            console.log('Player win')
+        } else {
+            gameOver = true
+        }
+        if (gameBoard[0][2] == 'X' && gameBoard[1][1] == 'X' && gameBoard[2][0] == 'X') {
+            console.log('Player win')
+        } else {
+            gameOver = true
+        }
+        if (gameBoard[0][1] == 'X' && gameBoard[1][1] == 'X' && gameBoard[2][1] == 'X') {
+            console.log('Player win')
+        } else {
+            gameOver = true
+        }
+        if (gameBoard[0][0] == 'X' && gameBoard[1][0] == 'X' && gameBoard[2][0] == 'X') {
+            console.log('Player win')
+        } else {
+            gameOver = true
+        }
+        if (gameBoard[0][2] == 'X' && gameBoard[1][2] == 'X' && gameBoard[2][2] == 'X') {
+            console.log('Player win')
+        } else {
+            gameOver = true
+        }
+        console.log(gameBoard)
+
     })
 });
